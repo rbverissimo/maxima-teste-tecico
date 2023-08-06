db2c0412510b2d978b09f6e6bd975516784fb80b
rbverissimo <rverissimo.dev@gmail.com>
2023-08-06 18:22:32 -0300

    Definindo o routing da aplicação para reconhecer todas as rotas possíveis e páginas de erro

--- logistica-web/src/app/app-routing.module.ts
+++ logistica-web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './negocio/cadastro/cadastro.component';
+import { EditarClienteComponent } from './negocio/cadastro/editar-cliente/editar-cliente.component';
+import { PaginaNaoEncontradaComponent } from './negocio/pagina-nao-encontrada/pagina-nao-encontrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/cadastro', pathMatch: 'full'},
+  {path: 'cadastro', component: CadastroComponent, children: [
+    {path: 'novo', component: EditarClienteComponent}
+  ]},
+  {path: 'nao-encontrado', component: PaginaNaoEncontradaComponent},
+  {path: '**', redirectTo: '/nao-encontrado' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- logistica-web/src/app/app.module.ts
+++ logistica-web/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './negocio/cadastro/cadastro.component';
 import { EditarClienteComponent } from './negocio/cadastro/editar-cliente/editar-cliente.component';
+import { PaginaNaoEncontradaComponent } from './negocio/pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
-    EditarClienteComponent
+    EditarClienteComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
