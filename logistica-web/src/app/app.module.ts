24faa0b8f49a04abecc75413952f81b3d00eb25d
rbverissimo <rverissimo.dev@gmail.com>
2023-08-06 19:01:48 -0300

    Adicionando as dependências ao Angular Material e ao bootstrap

--- logistica-web/src/app/app.module.ts
+++ logistica-web/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './negocio/cadastro/cadastro.component';
 import { EditarClienteComponent } from './negocio/cadastro/editar-cliente/editar-cliente.component';
 import { PaginaNaoEncontradaComponent } from './negocio/pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { HeaderComponent } from './negocio/header/header.component';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
     EditarClienteComponent,
-    PaginaNaoEncontradaComponent
+    PaginaNaoEncontradaComponent,
+    HeaderComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
