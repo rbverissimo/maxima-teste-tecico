fdbd0f2f912d9b8b99f827356258e4cbb798a0f0
rbverissimo <rverissimo.dev@gmail.com>
2023-08-06 22:14:09 -0300

    Implementação da primeira tabela, preparando para criar a paginação do sistema.

--- logistica-web/src/app/app.module.ts
+++ logistica-web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- logistica-web/src/app/negocio/cadastro/cadastro.component.ts
+++ logistica-web/src/app/negocio/cadastro/cadastro.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/modelos/cliente.model';
+import { Endereco } from 'src/app/modelos/endereco.model';
+
+
+const CLIENTES_DATA: Cliente[] = [
+  new Cliente(
+    1, 
+    'Plásticos Melhoramentos', 
+    '00.000.000/0000-00', 
+    new Endereco(1, 'Rua 1', '74200-000', 'ED BUSINESS')
+  ),
+  new Cliente(
+    2,
+    'Lâmpadas Eletrônicos',
+    '11.111.111/111-11',
+    new Endereco(1, 'Rua 1', '74200-000', 'ED JAVA BUSINESS')
+  ),
+  new Cliente(
+    3,
+    'Acabamentos Soares',
+    '12.333.123/100-00',
+    new Endereco(1, 'Rua 4', '74200-130', '')
+  ),
+    new Cliente(
+    7,
+    'Assessoria Maxima',
+    '17.145.133/027-81',
+    new Endereco(1, 'Rua 1', '74200-000', 'ED BUSINESS')
+  )
+];
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit, AfterViewInit {
 
+  displayedColumns: string[] = ['codigo', 'nome', 'cnpj', 'endereco'];
+  dataSource = CLIENTES_DATA;
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.dataSource);
+  }
 }
