3c1ec1b03bbe6288e2fb6d33bf939e0693253aaa
rbverissimo <rverissimo.dev@gmail.com>
2023-08-07 20:14:09 -0300

    Primero commit da primeira versão em serviço da API RESTful

---
+++ logistica-api/src/test/java/br/com/maximatech/logisticaapi/LogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maximatech.logisticaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
