baaec012d577bffcf6f24bbac7e92829a3361fc5
rbverissimo <rverissimo.dev@gmail.com>
2023-08-07 21:30:31 -0300

    Corrigindo bug à cerca do JpaRepository como interface

--- logistica-api/src/main/java/br/com/maximatech/logisticaapi/repositories/ClienteRepository.java
+++ logistica-api/src/main/java/br/com/maximatech/logisticaapi/repositories/ClienteRepository.java
@@ -1,14 +1,9 @@
 package br.com.maximatech.logisticaapi.repositories;
 
-import org.springframework.data.jpa.repository.support.JpaEntityInformation;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.maximatech.logisticaapi.model.Cliente;
-import jakarta.persistence.EntityManager;
 
-public class ClienteRepository extends SimpleJpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-	public ClienteRepository(JpaEntityInformation<Cliente, ?> entityInformation, EntityManager entityManager) {
-		super(entityInformation, entityManager);
-	}
 }
--- logistica-api/src/main/java/br/com/maximatech/logisticaapi/service/ClienteService.java
+++ logistica-api/src/main/java/br/com/maximatech/logisticaapi/service/ClienteService.java
@@ -2,13 +2,18 @@ package br.com.maximatech.logisticaapi.service;
 
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import br.com.maximatech.logisticaapi.model.Cliente;
 import br.com.maximatech.logisticaapi.repositories.ClienteRepository;
 
+@Service
 public class ClienteService {
 	
 	private Logger logger = Logger.getLogger(ClienteService.class.getName());
 	
+	@Autowired
 	ClienteRepository repository;
 	
 	public Cliente create(Cliente cliente) {
