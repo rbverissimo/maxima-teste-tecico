3e16f31943ca215d6d1fcb9691da6428a2d70af4
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2023-08-09 19:57:08 -0300

    Organizando imports

--- logistica-api/src/main/java/br/com/maximatech/logisticaapi/service/ClienteService.java
+++ logistica-api/src/main/java/br/com/maximatech/logisticaapi/service/ClienteService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
